c7995ec7d193299df86c353e86bd6e15ba09034f
Cab6701 <trinhgiachang@gmail.com>
2025-10-25 22:13:16 +0700

    clean code

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,26 +1,12 @@
-using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
-using WaterService.Models;
 
 namespace WaterService.Controllers;
 
 public class HomeController : Controller
 {
-    private readonly ILogger<HomeController> _logger;
-
-    public HomeController(ILogger<HomeController> logger)
-    {
-        _logger = logger;
-    }
-
     public IActionResult Index()
     {
         // Redirect to WaterInquiry page for direct customer access
         return RedirectToAction("WaterInquiry", "Account");
-    }
-
-    public IActionResult Privacy()
-    {
-        return View();
     }
 }
--- Program.cs
+++ Program.cs
@@ -7,7 +7,6 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-builder.Services.AddRazorPages();
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlite(
     builder.Configuration.GetConnectionString("DefaultConnection"))
 );
@@ -51,6 +50,5 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}")
     .WithStaticAssets();
-
 
 app.Run();
